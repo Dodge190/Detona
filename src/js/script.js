e8d29e771fcf7d72880c8b8a51ee3fc41b04ceee
Dodge190 <pedrohoxsantana@gmail.com>
2024-10-04 17:02:51 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-alert("Se o jogo não carregar ao clicar em 'Jogar', por favor, atualize a página e tente novamente."");
+alert("Se o jogo não carregar ao clicar em 'Jogar', por favor, atualize a página e tente novamente.");
 
 import Start from './modules/classes/Start.js';
 
